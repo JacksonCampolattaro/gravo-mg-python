fb06c970d3aeee85f2f376ce3516cd15c6d13858
Jackson Campolattaro <jackson.campolattaro@gmail.com>
2024-03-05 11:03:02 +0100

    Fix a mismatched type issue with sampling

--- src/gravomg/sampling.py
+++ src/gravomg/sampling.py
@@ -1,13 +1,13 @@
 import numpy
 
 import gravomg_bindings
+import scipy
 
 
-def fast_disc_sample(pos: numpy.ndarray, edges: numpy.ndarray, radius: float):
+def fast_disc_sample(pos: numpy.ndarray, edges: scipy.sparse.csr_matrix, radius: float):
     return gravomg_bindings.fast_disc_sample(pos, edges, radius)
 
 
 def fast_disc_sample_by_approximate_ratio(pos: numpy.ndarray, edges: numpy.ndarray, ratio: float):
     radius = gravomg_bindings.average_edge_length(pos, edges) * (ratio ** (1.0 / 3))
-    print(gravomg_bindings.average_edge_length(pos, edges), radius)
     return gravomg_bindings.fast_disc_sample(pos, edges, radius)
